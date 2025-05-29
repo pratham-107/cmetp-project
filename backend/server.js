bc519ec09c3d8b49e10a83c4f1ea968634b0ea60
pratham-107 <spratham845@gmail.com>
2025-05-29 18:16:11 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-
-const eventRoutes = require("./routes/eventRoutes");
-const adminRoutes = require("./routes/adminRoutes");
-
 const app = express();
+
+// Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+}));
 
 // Routes
 app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/events", eventRoutes);
-app.use("/api/admin", adminRoutes);
+app.use("/api/events", require("./routes/eventRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
 
+// DB Connect & Start Server
 mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => {
-    app.listen(5000, () => console.log("Server running on port 5000"));
+  .connect(process.env.MONGO_URI, {
   })
-  .catch((err) => console.log(err));
+  .then(() => {
+    app.listen(5000, () => console.log(`Server running on port 5000`));
+  })
+  .catch((err) => console.log("MongoDB connection error:", err));
